02b04e11a09a43c79fcefb55cbd6a139d3137b6d
B-Bala8055 <8055balalee@gmail.com>
2024-08-15 22:14:15 +0530

    Application init

---
+++ application/src/app/page.js
@@ -0,0 +1,11 @@
+// import styles from "./page.module.css";
+import 'bootstrap/dist/css/bootstrap.css';
+export default function Home() {
+  return (
+    <main>
+      <h1>
+        hello
+      </h1>
+    </main>
+  );
+}
